29bd500b42f825a81c01e1539d3e2e51f87d5b9f
zhoommer <akiff.fazill@gmail.com>
2025-04-20 01:58:38 +0300

    created application menu bar

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,8 +1,24 @@
 let date = new Date();
-
 let dateSpan = document.getElementById("date");
-let timeSpan = document.getElementById("time");
 
 dateSpan.innerText = date.toLocaleDateString();
-timeSpan.innerText = date.toLocaleTimeString();
+
+
+function updateClock() {
+  const clockElement = document.getElementById('time');
+  const date = new Date();
+  let hours = date.getHours();
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
+  hours = hours % 12 || 12;
+
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  clockElement.textContent = `${hours}:${minutes}:${seconds} ${ampm}`;
+}
+
+updateClock();
+// NOTE: Update clock every seconds
+setInterval(updateClock, 1000);
 
